cca91c1f89eade2a4c8ec14688db73550765e542
Nick Balestra <nick@balestra.ch>
2015-11-03 14:00:32 -0800

    ES6 cleanup

--- app/index.js
+++ app/index.js
@@ -1,3 +1,6 @@
+// For a more classic ES5 example of this very same file check out
+// https://github.com/nickbalestra/webpack-react-starter-kit/blob/master/app/index.js
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 
@@ -11,7 +14,9 @@ class Home extends React.Component {
   }
 
   render () {
-    return <h1>Hello {this.state.subject}</h1>
+    return (
+      <h1>Hello {this.state.subject}</h1>
+    )
   }
 }
 
